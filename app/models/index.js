a2488dab707627d4fd0c2136911eebfb103c37d2
MarkBezrukaviy <mark.bezrukaviy@gmail.com>
2017-04-22 22:26:03 +0300

    Fix

--- app/models/index.js
+++ app/models/index.js
@@ -1,25 +1,27 @@
-let fs = require('fs'),
-  path = require('path'),
-  Sequelize = require('sequelize'),
-  config = require('../../config/config'),
-  db = {};
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null;
 
-let sequelize = new Sequelize(config.db);
-
-fs.readdirSync(__dirname).filter(function (file) {
-  return (file.indexOf('.') !== 0) && (file !== 'index.js');
-}).forEach(function (file) {
-  let model = sequelize['import'](path.join(__dirname, file));
-  db[model.name] = model;
-});
-
-Object.keys(db).forEach(function (modelName) {
-  if ('associate' in db[modelName]) {
-    db[modelName].associate(db);
+  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize('example-app-db', 'root', null)
   }
-});
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    Students:      sequelize.import(__dirname + '/students')
+    // add your other models here
+  }
+}
 
-module.exports = db;
+module.exports = global.db
