9d3d4ffb75bee0bcfcf53338ca19f8ad6c6cb786
MarkBezrukaviy <mark.bezrukaviy@gmail.com>
2017-04-22 22:18:13 +0300

    Fix

--- config/express.js
+++ config/express.js
@@ -15,11 +15,11 @@ module.exports = function(app, config) {
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
 
-  pg.defaults.ssl = true;
+  /*pg.defaults.ssl = true;
   pg.connect(configDb.db, function(err) {
     if (err) throw err;
     console.log('Connected to postgres! Getting schemas...');
-  });
+  });*/
 
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'jade');
